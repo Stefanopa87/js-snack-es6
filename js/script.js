b90a789e2b8717d85678272494cb5776bc1fafc8
Stefano Patti <mastepa@hotmail.com>
2021-03-26 18:06:31 +0100

    secondo es done

--- js/script.js
+++ js/script.js
@@ -28,6 +28,54 @@ function es1(){
   console.log(small);
 }
 
+//  ------------------------------------------
+
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà:
+// nome, punti fatti, falli subiti.
+// Nome sarà l'unica proprietà da compilare,
+// le altre saranno tutte settate a 0.
+
+// Generare numeri random al posto degli 0 nelle proprietà:
+// punti fatti e falli subiti
+function random (min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function es2(){
+
+
+  let teams = [
+    {nome:'roma', punti:0, falli:0},
+    {nome:'lazio', punti:0, falli:0},
+    {nome:'juve', punti:0, falli:0},
+    {nome:'inter', punti:0, falli:0}];
+
+  for (let i = 0; i < teams.length; i++) {
+
+    // tiro fuori le key
+    let result = teams[i];
+    // assegno random a key
+    result.punti = random(0,10);
+    result.falli = random(0,20);
+  }
+  console.log(teams);
+
+  // Usando la destrutturazione creiamo un nuovo array
+  // i cui elementi contengono solo nomi e falli subiti
+  // e stampiamo tutto in console.
+
+  let newTeams = [];
+
+  for (let i = 0; i < teams.length; i++) {
+
+    let {punti, falli} = teams[i];
+
+    let insert = {punti, falli};
+    newTeams.push(insert)
+  }
+  console.log(newTeams);
+}
 
 function init(){
 
