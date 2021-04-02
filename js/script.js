aeb4dd287126f88345092fc4e7f4498741e1dd05
Stefano Patti <mastepa@hotmail.com>
2021-04-02 18:05:52 +0200

    add html done/ color done/ select done

--- js/script.js
+++ js/script.js
@@ -77,13 +77,200 @@ function es2(){
   console.log(newTeams);
 }
 
+
+// -------------------------------------------
+
+// Milestone 1
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+// Milestone 2
+// Coloriamo le icone per tipo
+// Milestone 3
+// Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+
+function es3(){
+
+  function getIconsDb() {
+    return [
+      {
+        name: "cat",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas",
+      },
+      {
+        name: "crow",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas",
+      },
+      {
+        name: "dog",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas",
+      },
+      {
+        name: "dove",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas",
+      },
+      {
+        name: "dragon",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas",
+      },
+      {
+        name: "horse",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas",
+      },
+      {
+        name: "hippo",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas",
+      },
+      {
+        name: "fish",
+        prefix: "fa-",
+        type: "animal",
+        family: "fas",
+      },
+      {
+        name: "carrot",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas",
+      },
+      {
+        name: "apple-alt",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas",
+      },
+      {
+        name: "lemon",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas",
+      },
+      {
+        name: "pepper-hot",
+        prefix: "fa-",
+        type: "vegetable",
+        family: "fas",
+      },
+      {
+        name: "user-astronaut",
+        prefix: "fa-",
+        type: "user",
+        family: "fas",
+      },
+      {
+        name: "user-graduate",
+        prefix: "fa-",
+        type: "user",
+        family: "fas",
+      },
+      {
+        name: "user-ninja",
+        prefix: "fa-",
+        type: "user",
+        family: "fas",
+      },
+      {
+        name: "user-secret",
+        prefix: "fa-",
+        type: "user",
+        family: "fas",
+      },
+    ];
+  }
+
+  const icons = getIconsDb();
+
+  print(icons);
+  // creo una funzione per scrivere codice in html
+  function print(array){
+    // mi aggancio al div classe icons dell'html
+    const container = $('.icons');
+
+
+
+    // creo un ciclo con forEach sugli elementi contenuti nell'array
+    array.forEach((elem) => {
+
+      // sezione colore da ciclare per inserire color in style html
+      let color;
+      if(elem.type === 'animal'){
+        color = 'orange';
+      } else if (elem.type === 'vegetable'){
+        color = 'blue';
+      } else if (elem.type === 'user'){
+        color = 'red';
+      }
+      // fine sezione colore
+
+      const iconHTML =
+      // con i pedici vado a fare un copia incolla in html, sostituendo le classi
+      // ad ogni ciclo in base a quello che mi interessa
+      // sostituiso quindi fas fa-*** con quello che mi interessa (attenzione agli spazi)
+      // .prefix e .name infatti devono essere vicini
+      `
+        <div>
+          <i class="${elem.family} ${elem.prefix}${elem.name}" style="color:${color}"></i>
+          <div class="title">${elem.name.toUpperCase()}</div>
+        </div>
+      `;
+      // appendo quindi quanto creato con il .append()
+      container.append(iconHTML);
+
+    });
+  }
+
+  addOptions(icons);
+
+  function addOptions(array){
+
+    // prima filtro i types
+    const types = [];
+    array.forEach(elem =>{
+
+      if(!types.includes(elem.type)){
+        types.push(elem.type)
+      }
+    });
+    console.log(types);
+    // fine filtro
+
+    const select = $('#type')
+
+    types.forEach(elem => {
+
+      const iconHTML =
+      `
+        <option value="${elem}">${elem}</option>
+      `;
+
+      select.append(iconHTML);
+    });
+  }
+}
+
 function init(){
 
-  es1();
+  // es1();
 
   console.log('primo es ^  --------------------- secondo es v');
 
-  es2();
+  // es2();
+
+  console.log('es pasqua');
+
+  es3();
 }
 
 
